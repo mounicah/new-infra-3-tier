b013d311a6cce6e5e82dc985bb61b464dcdf9eaa
mounicah <78117588+mounicah@users.noreply.github.com>
2026-06-01 16:41:09 -0500

    Change key_name in aws_launch_template resource
    updating key and ami id.

--- webasg.tf
+++ webasg.tf
@@ -16,7 +16,7 @@ resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
   image_id      = "ami-07a00cf47dbbc844c"
   instance_type = "t3.micro"
-  key_name      = "kubeadmm"
+  key_name      = "demo"
   network_interfaces {
     associate_public_ip_address = true
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg.id]
