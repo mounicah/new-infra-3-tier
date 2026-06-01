e6c0eb6f86341a23df68a015645f1d7983cf7b21
mounicah <78117588+mounicah@users.noreply.github.com>
2026-06-01 16:42:55 -0500

    Change key_name in aws_launch_template resource

--- appasg.tf
+++ appasg.tf
@@ -22,7 +22,7 @@ resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
   image_id      = "ami-07a00cf47dbbc844c"
   instance_type = "t3.micro"
-  key_name      = "kubeadmm"
+  key_name      = "demo"
 
   network_interfaces {
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
